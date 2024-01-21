e7d0778ab81c1e879a68f79af7cc0d876c4d87cd
Infamous003 <arfathsyed0312@gmail.com>
2024-01-21 18:17:44 +0530

    Fix styles not applying to new grids.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let gridContainerElement = document.querySelector('.grid-container');
 let gridArray = [];
 
+function getGridSizeFromUser() {
+  let inputElement = document.querySelector('input');
+  let size = (inputElement.value % 2 == 0)? inputElement.value : (inputElement.value + 1);
+  return size;
+}
+
 function setGridContainerStyles(size) {
   gridContainerElement.style.cssText = `
   display:grid;
@@ -8,45 +14,53 @@ function setGridContainerStyles(size) {
   width: 400px;
   height: 400px;
   grid-template-columns: repeat(${size}, 1fr);
+  grid-template-rows: repeat(${size}, 1fr);
   `;
 }
 
-function createGrid(rows, columns) {
-  console.log(gridArray);
-
-  setGridContainerStyles(rows);
-
+function createGrid(rows) {
   if(gridArray.length === 0) {
     for(let i = 0; i < rows; i++){
-      for(let j = 0; j < columns; j++){
+      for(let j = 0; j < rows; j++){
         const box = document.createElement('div');
         gridArray.push(box);
       }
     }
-    console.log('Grid created of size 5x5');
+    // if(!gridContainerElement.style.cssText) {
+    //   setGridContainerStyles(rows);
+    // }
+    return gridArray;
+  }else {
+    gridArray.splice(0, gridArray.length);
     return gridArray;
   }
 }
 
 function drawGrid() {
-  const grid = createGrid(16, 16);
-  console.log(grid.length);
-
-  grid.forEach(gridBox => {
-    gridContainerElement.appendChild(gridBox);
-    gridBox.style.cssText = `
-      border: 1px solid black;
-      `;
-    gridBox.classList.add('box');
-  });
-  checkMouseOnGrid();
-  return grid;
+  const grid = createGrid(getGridSizeFromUser()) || null;
+  console.log('This is a nodelist!' + grid);
+  gridContainerElement.innerHTML = '';
+  setGridContainerStyles(getGridSizeFromUser());
+  if(grid !== null){
+    console.log('This is a nodelist! BUt inside IF');
+    grid.forEach(gridBox => {
+      gridContainerElement.appendChild(gridBox);
+      gridBox.style.cssText = `
+        border: 1px solid black;
+        `;
+      gridBox.classList.add('box');
+      
+    });
+    if(!gridContainerElement.style.cssText) {
+      
+    }
+    checkMouseOnGrid();
+    return grid;
+  }
 }
 
 function checkMouseOnGrid() {
   let gridArray = document.querySelectorAll('.box');
-  console.log(gridArray);
-
   gridArray.forEach((box) => {
     box.addEventListener('mouseenter', () => {
       box.style.backgroundColor = 'black';
